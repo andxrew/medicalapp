fc0d2f3107dd69cbcb460c79b69de6b3e1ab0ed4
Akashi9006 <malif9006@gmail.com>
2024-01-25 19:18:03 +0000

    update resource page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -13,9 +13,21 @@ const navigation = [
 const faqs = [
   {
     id: 1,
-    question: "What's the best thing about Switzerland?",
+    question: "What are some basic tips for maintaining good physical health?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "To maintain good physical health, prioritize regular exercise, consume a balanced diet rich in fruits and vegetables, stay hydrated, get adequate sleep, and avoid harmful habits such as smoking and excessive alcohol consumption.",
+  },
+  {
+    id: 2,
+    question: " What are some effective ways to improve mental well-being?",
+    answer:
+      "Focus on activities that bring joy and fulfillment, maintain social connections, engage in regular physical activity, get sufficient sleep, and consider mindfulness practices like meditation to enhance mental well-being.",
+  },
+  {
+    id: 3,
+    question: "How can I enhance my overall quality of life?",
+    answer:
+      "Improving your overall quality of life involves a holistic approach. Focus on maintaining a healthy balance between physical, mental, and social well-being. Prioritize self-care, cultivate positive relationships, and engage in activities that bring you joy and fulfillment.",
   }
 ]
   // More questions...
@@ -109,13 +121,13 @@ export default function Resources() {
         </header>
         {/*FAQ */}
       <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
-        <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
+        <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">General Health And Resources</h2>
         <p className="mt-6 max-w-2xl text-base leading-7 text-gray-600">
-          Have a different question and can’t find the answer you’re looking for? Reach out to our support team by{' '}
-          <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-            sending us an email
+        For frequently asked questions and additional resources, please refer to our curated collection. For more extensive assistance, you can explore the {' '}
+          <a href="https://www.nhs.uk/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+          NHS website
           </a>{' '}
-          and we’ll get back to you as soon as we can.
+          for comprehensive information and support.
         </p>
         <div className="mt-20">
           <dl className="space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:grid-cols-3 lg:gap-x-10">
@@ -128,7 +140,7 @@ export default function Resources() {
           </dl>
         </div>
       </div>
-      {/* extra 1 */}
+      {/* Physical Health Tab */}
       <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
       <div className="absolute inset-0 -z-10 overflow-hidden">
         <svg
@@ -160,11 +172,91 @@ export default function Resources() {
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
-              <p className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
-              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
+              <p className="text-base font-semibold leading-7 text-indigo-600"></p>
+              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Physical Health</h1>
+              <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Nutrition</h2>
               <p className="mt-6 text-xl leading-8 text-gray-700">
-                Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
-                eget aliquam. Quisque id at vitae feugiat egestas.
+              The {' '} 
+              <a href="https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/the-eatwell-guide/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+             NHS Eatwell Guide
+              </a>{' '}
+              offers practical tips on creating a balanced diet. Learn about portion sizes, incorporating diverse fruits and vegetables, and the importance of staying hydrated for overall health.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+  <img
+    style={{ maxWidth: '60%', height: 'auto' }}
+    className="w-[24rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+    src="https://images.pexels.com/photos/5327864/pexels-photo-5327864.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    alt=""
+  />
+</div>
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+            <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Excerise and Fitness</h2>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+              Explore the {' '}
+              <a href="https://www.nhs.uk/conditions/nhs-fitness-studio/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              NHS Fitness Studio
+              </a>{' '}
+              for a variety of workout routines. Get personalized tips on staying active, whether through quick home exercises or outdoor activities, making fitness an accessible part of your routine.
+              </p>
+              <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Hygiene and Personal Care</h2>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+              <a href="https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/the-eatwell-guide/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              NHS Live Well
+              </a>{' '}
+              gives practical tips for personal hygiene. Beyond basics, discover habits that prevent illnesses and contribute to an overall sense of well-being.              
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* Physical Health Tab */}
+      <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        <svg
+          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+        </svg>
+      </div>
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="lg:max-w-lg">
+              <p className="text-base font-semibold leading-7 text-indigo-600"></p>
+              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Physical Health</h1>
+              <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Nutrition</h2>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+              The {' '} 
+              <a href="https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/the-eatwell-guide/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+             NHS Eatwell Guide
+              </a>{' '}
+              offers practical tips on creating a balanced diet. Learn about portion sizes, incorporating diverse fruits and vegetables, and the importance of staying hydrated for overall health.
               </p>
             </div>
           </div>
@@ -179,47 +271,20 @@ export default function Resources() {
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-              <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
-                vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque
-                erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-                semper sed amet vitae sed turpis id.
+            <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Excerise and Fitness</h2>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+              Explore the {' '}
+              <a href="https://www.nhs.uk/live-well/eat-well/food-guidelines-and-food-labels/the-eatwell-guide/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              NHS Fitness Studio
+              </a>{' '}
+              for a variety of workout routines. Get personalized tips on staying active, whether through quick home exercises or outdoor activities, making fitness an accessible part of your routine.
               </p>
-              <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                <li className="flex gap-x-3">
-                  <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                    blanditiis ratione.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
-                    ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <ServerIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                    vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-                  </span>
-                </li>
-              </ul>
-              <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
-              </p>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-              <p className="mt-6">
-                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
-                Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-                tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                turpis ipsum eu a sed convallis diam.
+              <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Hygiene and Personal Care</h2>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+              <a href="https://www.nhs.uk/live-well/" className="font-semibold text-indigo-600 hover:text-indigo-500">
+              NHS Live Well
+              </a>{' '}
+              gives practical tips for personal hygiene. Beyond basics, discover habits that prevent illnesses and contribute to an overall sense of well-being.              
               </p>
             </div>
           </div>
